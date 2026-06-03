2b27d28243689f06387b19891aa28188a5f2aab4
Sai-developer-6699 <shukruneai@gmail.com>
2026-06-03 12:00:00 +0530

    feat: implement tutor views and authentication APIs

---
+++ neurotutor/tutor/views_auth.py
@@ -0,0 +1,8 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
